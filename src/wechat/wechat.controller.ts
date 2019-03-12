0eda697275c1b65a57548aabfbd4bb80bc40c3f6
zhangjainxi <a867250586@yeah.net>
2019-03-12 23:19:28 +0800

    打印post接口

--- src/wechat/wechat.controller.ts
+++ src/wechat/wechat.controller.ts
@@ -5,15 +5,17 @@ import { create } from 'domain';
 
 @Controller('wechat')
 export class WechatController {
-    @Get()
+    @Get('/checkwx')
     async checkwx(@Param('echostr') echostr,@Req() request): Promise<any[]> {
-        var a = request.query.echostr;
-        console.log(request.query.echostr)
+        var a = echostr;
+        console.log(echostr)
         return a;
     }
 
     @Post()
     async create(@Req() request) {
+        console.log("这里是post的方法，为了获取微信的消息")
+
         let wxEventMsg = await new Promise(function (resolve, reject) {
             let buf = ''
             request.setEncoding('utf8')
