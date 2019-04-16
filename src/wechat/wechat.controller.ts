3b4278cf5d77683f9340087b1e0666b8776781e0
zhangjainxi <a867250586@yeah.net>
2019-04-16 15:29:50 +0800

    up

--- src/wechat/wechat.controller.ts
+++ src/wechat/wechat.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res, Post, Body, HttpStatus,Param } from '@nestjs/common';
+import { Controller, Get, Req, Res, Post, Body, HttpStatus, Param } from '@nestjs/common';
 import { finished } from 'stream';
 import { create } from 'domain';
 import { response } from 'express';
@@ -7,14 +7,14 @@ var fastXmlParser = require('fast-xml-parser');
 @Controller('wechat')
 export class WechatController {
     @Get('/checkwx')
-    async checkwx(@Param('echostr') echostr,@Req() request): Promise<any[]> {
+    async checkwx(@Param('echostr') echostr, @Req() request): Promise<any[]> {
         var a = echostr;
         console.log(echostr)
         return a;
     }
 
     @Post()
-    async create(@Req() request,@Res() response) {
+    async create(@Req() request, @Res() response) {
         console.log("这里是post的方法，为了获取微信的消息")
 
         let wxEventMsg = await new Promise(function (resolve, reject) {
@@ -27,14 +27,16 @@ export class WechatController {
                 resolve(buf)
             })
         })
+        let wxEventMsgJson: any
         console.log(wxEventMsg);
         parseString(wxEventMsg, function (err, result) {
-            console.dir(JSON.stringify(result));
+            // console.dir(JSON.stringify(result));
+            console.log(result, ">>>>>>>", err)
+            if (!err) {
+                wxEventMsgJson = result.xml;
+            }
         });
-        var wxEventMsgJson = fastXmlParser.getTraversalObj(wxEventMsg);
-        var wxEventMsgXml = fastXmlParser.convertToJson(wxEventMsgJson)
-        console.log(wxEventMsgJson,wxEventMsgXml)
-        response.setHeader('content-type', 'text/xml'); 
-        response.send("success"); 
+        response.setHeader('content-type', 'text/xml');
+        response.send("success");
     }
 }
