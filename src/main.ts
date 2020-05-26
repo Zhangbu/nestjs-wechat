0e06a94f7d9f18a3741feb4af4ba30fe0b57eec8
zhangjainxi <a867250586@yeah.net>
2020-05-26 10:26:34 +0800

    改端口8080

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8088);
-  console.log("监听8088端口");
+  await app.listen(8080);
+  console.log("监听8080端口");
 }
 bootstrap();
